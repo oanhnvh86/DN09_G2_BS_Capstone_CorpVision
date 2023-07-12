54e8284833e93317e9175f3e874995643e2b0306
Hien <hientnt0999@gmail.com>
2023-07-12 15:09:05 +0700

    update

--- js/search.js
+++ js/search.js
@@ -1,14 +1,13 @@
 
-function Openform(){
-    var open = document.getElementById("clickSearch");
-        open.classList.add("active")
+function Openform() {
+    var openI = document.getElementById("clickSearch");
+    openI.classList.toggle("active");
+    var openA = document.getElementById("aClickSearch");
+    openA.classList.toggle("active");
+    var openInp = document.getElementById("inpClickSearch");
+    openInp.classList.toggle("active");
 
 }
 document.getElementById("clickSearch").onclick = Openform;
 
-function Closeform(){
-    var closee = document.getElementById("clickSearch");
-    closee.classList.remove("active");
 
-}
-document.getElementById("closeSearch").onclick = Closeform;
